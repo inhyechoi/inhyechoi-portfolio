c56eb097df403dc3a717623d71be2cfc973e9563
inhyechoi <inhyechoi0404@gmail.com>
2019-10-10 14:23:22 -0700

    edited alignmnet on mainpage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import menutext1 from './assets/main-menu-text-1.svg';
 import menutext2 from './assets/main-menu-text-2.svg';
 import maintitle from './assets/inhye-world-text.svg';
 import companytitle from './assets/2019inhyechoi.png';
+import menuselect from './assets/menu-select.svg';
 import './App.scss';
 import './mixin.scss';
 import './variables.scss';
@@ -13,18 +14,30 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-            <img src={maintitle} className="main-title" alt="" />
-            <img src={menutext1} className="menu-text-1" alt="" />
-            <img src={menutext2} className="menu-text-2" alt="" />
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <img src={companytitle} className="company-title" alt="" />
+        <div className="App-header-text-container">
+            <img src={maintitle} className="main-title" alt="" style={{ width: '100%' }}/>
+            <div>
+                <div className="menu-text-wrapper">
+                    <img src={menuselect} className="menu-select" alt="" />
+                    <img src={menutext1} className="menu-text-1" alt="" />
+                </div>
+                <div className="menu-text-wrapper">
+                    <img src={menuselect} className="menu-select-2" alt="" />
+                    <img src={menutext2} className="menu-text-2" alt="" />
+                </div>
+            </div>
+
+
+            {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+            </a> */}
+            <img src={companytitle} className="company-title" alt="" />
+            </div>    
       </header>
 
     </div>

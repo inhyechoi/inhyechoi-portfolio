083310c2bbf99ae261b328bcc76bee1f25985c73
inhyechoi <inhyechoi0404@gmail.com>
2019-10-10 13:13:28 -0700

    edited main page bgimage and added text image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import menutext1 from './assets/main-menu-text-1.svg';
+import menutext2 from './assets/main-menu-text-2.svg';
+import maintitle from './assets/inhye-world-text.svg';
+import companytitle from './assets/2019inhyechoi.png';
+import './App.scss';
 import './mixin.scss';
 import './variables.scss';
 
@@ -9,9 +13,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          1 Player Game: Inhye Choi
-        </p>
+            <img src={maintitle} className="main-title" alt="" />
+            <img src={menutext1} className="menu-text-1" alt="" />
+            <img src={menutext2} className="menu-text-2" alt="" />
         {/* <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +24,9 @@ function App() {
         >
           Learn React
         </a> */}
+        <img src={companytitle} className="company-title" alt="" />
       </header>
+
     </div>
   );
 }

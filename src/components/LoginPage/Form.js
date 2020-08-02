8caf849bb83fa2be25b3592e3eb4e6025986fd4d
inhyechoi <inhyechoi0404@gmail.com>
2020-08-01 20:09:59 -0700

    added default values and error msg for form on login page

--- src/components/LoginPage/Form.js
+++ src/components/LoginPage/Form.js
@@ -2,9 +2,13 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 const Form = () => {
-    const { handleSubmit, errors } = useForm();
-    const onSubmit = (data)=>{
-        console.log(data)
+    const { handleSubmit, errors, register } = useForm({
+        defaultValues:{
+            firstName: ""
+        }
+    });
+    const onSubmit = data =>{
+        console.log(data);
     };
     return(
     /* "handleSubmit" will validate input before invoking "onSubmit" */
@@ -12,11 +16,15 @@ const Form = () => {
         <form onSubmit={handleSubmit(onSubmit)} className="namebox">
         <p>Welcome, what is your name?</p>
         <div className="form-box">
-        <input className="textbox" type="name" placeholder="Your name" aria-invalid={errors.name ? "true" : "false"} />
-            {errors.name && errors.name.type === 'Yourname is required'}
-            {errors.name && errors.name.type === 'Max length exceeded'}
+        <input className="textbox" type="text" name="firstName" placeholder="Your name" ref={register({ required: true })}/>
+            {errors.firstName && errors.firstName.type === "required" && (
+                <div className="error-name">This is required.</div>
+            )}
     {/* requiring assets directly from jsx if bundling code from the server-side*/}
-        <input className='submitbox' type="image" src={require('../../assets/icon-mushroom.png')} alt='Submit'/>
+        {/* <input className='submitbox' type="image" name="name" src={require('../../assets/icon-mushroom.png')} alt='Submit' ref={register} onSubmit={ e => {e.preventDefault();}}/>
+            {errors.firstName && errors.firstName.type === "required" && (
+                <div className="error">You must enter your name.</div>
+            )} */}
         </div>
         </form>
     </div>

cf4f8355f09f568d4c46a483a1a2a1d91be05e64
inhyechoi <inhyechoi0404@gmail.com>
2020-08-09 09:55:21 -0700

    added loader component

--- src/components/LoginPage/Form.js
+++ src/components/LoginPage/Form.js
@@ -11,23 +11,23 @@ const Form = () => {
         console.log(data);
     };
     return(
-    /* "handleSubmit" will validate input before invoking "onSubmit" */
-    <div className="form">
-        <form onSubmit={handleSubmit(onSubmit)} className="namebox">
-        <p>Welcome, what is your name?</p>
-        <div className="form-box">
-        <input className="textbox" type="text" name="firstName" placeholder="Your name" ref={register({ required: true })}/>
-            {errors.firstName && errors.firstName.type === "required" && (
-                <div className="error-name">Username is incorrect.<br />Try again!</div>
-            )}
-    {/* requiring assets directly from jsx if bundling code from the server-side*/}
-        {/* <input className='submitbox' type="image" name="name" src={require('../../assets/icon-mushroom.png')} alt='Submit' ref={register} onSubmit={ e => {e.preventDefault();}}/>
-            {errors.firstName && errors.firstName.type === "required" && (
-                <div className="error">You must enter your name.</div>
-            )} */}
+        /* "handleSubmit" will validate input before invoking "onSubmit" */
+        <div className="form">
+            <form onSubmit={handleSubmit(onSubmit)} className="namebox">
+            <p>Welcome, what is your name?</p>
+            <div className="form-box">
+                <input className="textbox" type="text" name="firstName" placeholder="Your name" ref={register({ required: true })}/>
+                    {errors.firstName && errors.firstName.type === "required" && (
+                        <div className="error-name">Username is incorrect.<br />Try again!</div>
+                    )}
+            {/* requiring assets directly from jsx if bundling code from the server-side*/}
+                {/* <input className='submitbox' type="image" name="name" src={require('../../assets/icon-mushroom.png')} alt='Submit' ref={register} onSubmit={ e => {e.preventDefault();}}/>
+                    {errors.firstName && errors.firstName.type === "required" && (
+                        <div className="error">You must enter your name.</div>
+                    )} */}
+            </div>
+            </form>
         </div>
-        </form>
-    </div>
     );
 }
 export default Form
---
+++ src/components/LoginPage/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loader = () => {
+    return(
+        <div class="folding-loader">
+            <div class="folding-cube1 folding-cube"></div>
+            <div class="folding-cube2 folding-cube"></div>
+            <div class="folding-cube4 folding-cube"></div>
+            <div class="folding-cube3 folding-cube"></div>
+        </div>
+    );
+}
+
+export default Loader

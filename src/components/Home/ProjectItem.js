20222e76f023502339712c17067a00fe13d5212e
inhyechoi <inhyechoi0404@gmail.com>
2020-09-23 16:12:16 -0700

    fixed useWindowWidth error, created useWindowWidth hooks

--- src/components/Home/ProjectItem.js
+++ src/components/Home/ProjectItem.js
@@ -1,30 +1,23 @@
-import React,  { useState, useEffect } from 'react';
+import React from 'react';
 import './ProjectItem.scss';
+import useWindowWidth from '../../Hooks/useWindowWidth.js';
 
 import desktopImage from '../../Assets/Images/Projects/Desktop/JJEntertainment.jpg';
 import mobileImage from '../../Assets/Images/Projects/Mobile/JJEntertainment_changmo_jedharper_instagram_square.jpg'
 
+console.log(desktopImage);
 const ProjectItem = ({ desktopImage, title, viewProject }) => {
     const imageUrl = useWindowWidth() >= 650 ? desktopImage : mobileImage;
-    const useWindowWidth = () =>{
-    const [ windowWidth, setWindowWidth ] = useState(window.innerWidth);
 
-    useEffect(() => {
-        const handleWindowResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
-
-        window.addEventListener('resize', handleWindowResize);
-        return () => window.removeEventListener('resize', handleWindowResize);
-    },[]);
-    
-    return windowWidth;
-    };
-
+    const { windowWidth } = useWindowWidth();
 return(
-    <div className="projectBackgroundImage" style={{ desktopImage: `url(${imageUrl})`}}>
-        <div className="title">{title}</div> 
-        <div className="viewProject">{viewProject}</div>
+    <div className="projectBackgroundImage" style={{ backgroundImage: `url(${imageUrl})`}}>
+        {windowWidth >= 650 &&( 
+            <>
+            <div className="title">{title}</div> 
+            <div className="viewProject">{viewProject}</div>
+            </>
+        )}  
     </div>
 );
 }; 

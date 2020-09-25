dba67d5bdccfa26ec4a022f5ab0e0011dd86eac0
inhyechoi <inhyechoi0404@gmail.com>
2020-09-25 15:35:31 -0700

    working on dynamic background image

--- src/components/Home/ProjectItem.js
+++ src/components/Home/ProjectItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './ProjectItem.scss';
 import useWindowWidth from '../../Hooks/useWindowWidth.js';
+import { projects } from '../../data'
 
 import desktopImage from '../../Assets/Images/Projects/Desktop/JJEntertainment.jpg';
 import mobileImage from '../../Assets/Images/Projects/Mobile/JJEntertainment_changmo_jedharper_instagram_square.jpg'
 
-const ProjectItem = ({ title, viewProject }) => {
+
+const ProjectItem = ({ viewProject }) => {
+
     const imageUrl = useWindowWidth() >= 650 ? desktopImage : mobileImage;
 
     const { windowWidth } = useWindowWidth();
@@ -13,7 +16,15 @@ const ProjectItem = ({ title, viewProject }) => {
         <div className="projectItem" style={{ backgroundImage: `url(${ imageUrl })`}}>
             {windowWidth >= 650 &&( 
                 <>
-                <div className="title">{title}</div> 
+                <div className="title">
+                    {projects.map((data, key)=>{
+                    return(
+                        <div key={key}>
+                        {data.title}
+                        </div>
+                    );
+
+                })}</div> 
                 <div className="viewProject">{viewProject}</div>
                 </>
             )}  

d74e71d57e7311712a3efa529a146537cd1a9e5c
inhyechoi <inhyechoi0404@gmail.com>
2020-09-25 11:48:48 -0700

    removed destructure backgroundImage from ProjectItem's props

--- src/components/Home/ProjectItem.js
+++ src/components/Home/ProjectItem.js
@@ -5,22 +5,20 @@ import useWindowWidth from '../../Hooks/useWindowWidth.js';
 import desktopImage from '../../Assets/Images/Projects/Desktop/JJEntertainment.jpg';
 import mobileImage from '../../Assets/Images/Projects/Mobile/JJEntertainment_changmo_jedharper_instagram_square.jpg'
 
-console.log(desktopImage);
-console.log(mobileImage);
-const ProjectItem = ({ desktopImage, title, viewProject }) => {
+const ProjectItem = ({ title, viewProject }) => {
     const imageUrl = useWindowWidth() >= 650 ? desktopImage : mobileImage;
 
     const { windowWidth } = useWindowWidth();
-return(
-    <div className="projectItem" style={{ backgroundImage: `url(${ imageUrl })`}}>
-        {windowWidth >= 650 &&( 
-            <>
-            <div className="title">{title}</div> 
-            <div className="viewProject">{viewProject}</div>
-            </>
-        )}  
-    </div>
-);
+    return(
+        <div className="projectItem" style={{ backgroundImage: `url(${ imageUrl })`}}>
+            {windowWidth >= 650 &&( 
+                <>
+                <div className="title">{title}</div> 
+                <div className="viewProject">{viewProject}</div>
+                </>
+            )}  
+        </div>
+    );
 }; 
 
 export default ProjectItem

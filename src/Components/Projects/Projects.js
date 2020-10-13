7eed9fc2b7a42e0ff57b02935ab4c0ab2bfe8c73
inhyechoi <inhyechoi0404@gmail.com>
2020-10-12 18:51:09 -0700

    working on deploy error

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,21 +2,10 @@ import React from 'react';
 import ProjectItem from './ProjectItem.js';
 
 import './Projects.scss';
-// import { JJEntertainment } from '../../Assets/Images/Projects/Desktop'
 
 const Projects = () => {
-    // const projectsItemList =[
-    //     {
-    //         title: "JJEntertainment",
-    //         text: `Ux and Ui design for JJEntertainment's responsive website`,
-    //         backgroundimage: JJEntertainment,  
-    //     }
-    // ]
     return(
         <section className="projectsWrapper">
-            {/* {projectsItemList.map((item) =>(
-                <ProjectItem {...item} key={item.title}/>
-            ))} */}
             <div className="projectContainer">   
                     <h1 className="title">Projects</h1>  
                 <ProjectItem />

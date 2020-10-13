da08cfa04f744aef2e22e51a8c68c9787fcc4b44
inhyechoi <inhyechoi0404@gmail.com>
2020-10-13 15:54:04 -0700

    installed gooey dependencies, added blob in about.js

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,170 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Goo from 'gooey-react'
 
 const About = () => {
 
     return(
         <div className="about">
             <title>Inhye Choi</title>
-            <svg id="inhye-bg" width="100%" height="100%" xmlms="https://www.w3.org/TR/SVG/" viewBox="0 0 1295.87 804.79">
-            <defs>
-                <filter id="dropshadow">
-                <feDropShadow dx="0" dy="0" stdDeviation="1" floodColor="#E65772" />
-                </filter>
-            </defs>
-                <g id="rect-prism-1">
-                    <line x1="55" y1="30" x2="100" y2="10"  stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="30" x2="55" y2="48"  stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="100" y1="10" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="100" y1="10" x2="117" y2="12" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="117" y1="12" x2="124" y2="28" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="124" y1="28" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="50" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="50" x2="70" y2="55" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="70" y1="55" x2="124" y2="28" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    {/* <animateMotion
-                        attributeName="transform"
-                        begin="0s"
-                        dur="10s"
-                        path="M445.053 387c11.052 0 20.012 8.954 20.012 20s-8.96 20-20.012 20-20.012-8.954-20.012-20 8.96-20 20.012-20z"
-                        repeatCount="indefinite" 
-                        rotate="auto"/> */}
-                </g>
-                <g id="rect-prism-2">
-                    <line x1="55" y1="30" x2="100" y2="10"  stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="30" x2="55" y2="48"  stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="100" y1="10" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="100" y1="10" x2="117" y2="12" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="117" y1="12" x2="124" y2="28" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="124" y1="28" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="50" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="50" x2="70" y2="55" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="70" y1="55" x2="124" y2="28" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                </g>
-                <g id="rect-prism-3">
-                    <line x1="55" y1="30" x2="100" y2="10"  stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="30" x2="55" y2="48"  stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="100" y1="10" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="100" y1="10" x2="117" y2="12" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="117" y1="12" x2="124" y2="28" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="124" y1="28" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="50" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="50" x2="70" y2="55" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="70" y1="55" x2="124" y2="28" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                </g>
-                <g id="rect-prism-4">
-                    <line x1="55" y1="30" x2="100" y2="10"  stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="30" x2="55" y2="48"  stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="100" y1="10" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="100" y1="10" x2="117" y2="12" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="117" y1="12" x2="124" y2="28" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="124" y1="28" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="50" x2="107" y2="25" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="55" y1="50" x2="70" y2="55" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                    <line x1="70" y1="55" x2="124" y2="28" stroke="black" strokeWidth="2" strokeLinecap="round"/>
-                </g>
-                <g id="circle-1">
-                    <circle r="8" cy="300" cx="300" id="circle" />
-                    {/* <animateMotion 
-                        attributeName="transform"
-                        type="rotate"
-                        begin="0s"
-                        dur="10s"
-                        repeatCount="indefinite"
-                        path="M200,-500 C20,-50 380,150 380,50 C380-50 20,350 20,50 z"/> */}
-                </g>
-                <g id="circle-2">
-                    <circle r="8" cy="300" cx="300" id="circle" />
-                </g>
-                <g id="circle-3">
-                    <circle r="8" cy="300" cx="300" id="circle" />
-                </g>
-                <g id="circle-4">
-                    <circle r="8" cy="300" cx="300" id="circle"/>
-                </g>
-                <g id="spiral-1" width="100" height="100">
-                    <path fill="none" strokeWidth="6" strokeLinecap="round"
-                        d="M270 300 
-                        C 300.5 355 228 360.5 230 320 
-                        C 230 280 282 318 251.5 328.5
-                        C 242.5 328.5 234.5 315.5 249.5 314.5"/> 
-                </g>
-                <g id="spiral-2" width="100" height="100">
-                    <path fill="none" strokeWidth="6" strokeLinecap="round" d="M270 300 
-                    C 300.5 355 228 360.5 230 320 
-                    C 230 280 282 318 251.5 328.5
-                    C 242.5 328.5 234.5 315.5 249.5 314.5"/> 
-                </g>
-                <g id="spiral-3" width="100" height="100">
-                    <path fill="none" strokeWidth="6" strokeLinecap="round" d="M270 300 
-                    C 300.5 355 228 360.5 230 320 
-                    C 230 280 282 318 251.5 328.5
-                    C 242.5 328.5 234.5 315.5 249.5 314.5"/> 
-                </g>
-                <g id="spiral-4" width="100" height="100">
-                    <path fill="none" strokeWidth="6" strokeLinecap="round" d="M270 300 
-                    C 300.5 355 228 360.5 230 320 
-                    C 230 280 282 318 251.5 328.5
-                    C 242.5 328.5 234.5 315.5 249.5 314.5"/> 
-                </g>
-                <g id="curve-1" width="100" height="100">
-                <path fill="none" strokeWidth="2" d="M1083.5 309 
-                C 1067.5 295 1112 263.5 1110.5 293.5 
-                C 1110.5 318.5 1129.5 280.5 1125.5 289.5
-                C 1129.5 273.5 1163.5 284.5 1133.5 319.5
-                C 1120.5 326.5 1120.5 316.5 1127.5 308.5
-                C 1127.5 310.5 1140.5 288.5 1113.5 310.5
-                C 1104.5 319.5 1099.5 298.5 1100.5 300.5
-                C 1100.5 308.5 1099.5 275.5 1088.5 300.5
-                C 1088.5 310.5 1083.5 310.5 1083 308
-                "/>  
-                {/* <animateMotion
-                    attributeName="transform"
-                    begin="0s"
-                    dur="10s"
-                path="M-500, 100 C50,-580 500,150 150,300 C50"
-                    repeatCount="indefinite" 
-                    rotate="auto"/> */}
-                </g>
-                <g id="curve-2" width="100" height="100">
-                <path fill="none" strokeWidth="2" d="M1083.5 309 
-                C 1067.5 295 1112 263.5 1110.5 293.5 
-                C 1110.5 318.5 1129.5 280.5 1125.5 289.5
-                C 1129.5 273.5 1163.5 284.5 1133.5 319.5
-                C 1120.5 326.5 1120.5 316.5 1127.5 308.5
-                C 1127.5 310.5 1140.5 288.5 1113.5 310.5
-                C 1104.5 319.5 1099.5 298.5 1100.5 300.5
-                C 1100.5 308.5 1099.5 275.5 1088.5 300.5
-                C 1088.5 310.5 1083.5 310.5 1083 308
-                "/>  
-                </g>
-                <g id="curve-3" width="100" height="100">
-                <path fill="none" strokeWidth="2" d="M1083.5 309 
-                C 1067.5 295 1112 263.5 1110.5 293.5 
-                C 1110.5 318.5 1129.5 280.5 1125.5 289.5
-                C 1129.5 273.5 1163.5 284.5 1133.5 319.5
-                C 1120.5 326.5 1120.5 316.5 1127.5 308.5
-                C 1127.5 310.5 1140.5 288.5 1113.5 310.5
-                C 1104.5 319.5 1099.5 298.5 1100.5 300.5
-                C 1100.5 308.5 1099.5 275.5 1088.5 300.5
-                C 1088.5 310.5 1083.5 310.5 1083 308
-                "/>  
-                </g>
-                <p><Link to="/about">about</Link></p>
-                <foreignObject x="60%" y="70%" width="300" height="400" className="aboutHeader">
-                    <div xmlns="https://www.w3.org/TR/SVG/">
-                        <h1 className="title">
-                                <span>Hello I’m Inhye,a  </span>
-                                <span>Front-end Developer.</span>
-                        </h1>
-                        <h2 className="title">
-                            <span>My passion is in </span>
-                            <span>creative frontend </span>
-                        <span>development</span>
-                        </h2>
-                    </div>
-                </foreignObject>
+            <Goo intensity="weak">
+            <svg className="inhyeBgFilter">
+                <defs>
+                    <filter colorInterpolationFilters="sRGB" id="gooey-react">
+                        <feGaussianBlur stdDeviation="12"></feGaussianBlur>
+                        <feColorMatrix values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 72 -36">
+                        </feColorMatrix>
+                    </filter>
+                </defs>
             </svg>
+            <div className="inhyeBgWrapper">
+                <svg id="inhyeBg" width="100%" height="100%" xmlms="https://www.w3.org/TR/SVG/" viewBox="0 0 523.87 535.79">
+                    <g className="inhyeBg-g">
+                        <circle className="c-1" cx="50%" cy="50%" r="42" fill="lightseagreen"></circle>
+                        <circle className="c-2" cx="50%" cy="50%" r="36" fill="mediumaquamarine"></circle>  
+                        <circle className="c-3" cx="50%" cy="50%" r="30" fill="palegreen"></circle>
+                        <circle className="c-4" cx="50%" cy="50%" r="24" fill="mediumspringgreen"></circle>
+                    </g>
+                    {/* <p><Link to="/about">about</Link></p> */}
+                    <foreignObject x="0%" y="70%" width="300" height="400" className="aboutHeader">
+                        <div xmlns="https://www.w3.org/TR/SVG/">
+                            <h1 className="title">
+                                    <span>Hello I’m Inhye,a  </span>
+                                    <span>Front-end Developer.</span>
+                            </h1>
+                            <h2 className="title">
+                                <span>My passion is in </span>
+                                <span>creative frontend </span>
+                            <span>development</span>
+                            </h2>
+                        </div>
+                    </foreignObject>
+                </svg>
+            </div>
+            </Goo>
         </div>
     );
 }

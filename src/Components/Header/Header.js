88a4f690e3cf49fda25364f2bdea4bced61ce6aa
Inhye Choi <inhyechoi0404@gmail.com>
2021-03-16 12:15:10 -0700

    edited style on header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import DarkModeToggle from '../Theme/index.js';
 import { Link } from 'react-router-dom';
-import MobileMenu from './MobileMenu.js';
+// import MobileMenu from './MobileMenu.js';
+import { ReactComponent as CloseMenu } from "../../Assets/cross.svg";
+import { ReactComponent as MenuIcon } from "../../Assets/hamburger.svg"
+import { ReactComponent as Logo } from "../../Assets/inhye-main-logo.svg";
+import "./Header.scss";
 
 const Header = () => {
     
@@ -10,17 +14,99 @@ const Header = () => {
         document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
     }, false);
 
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return (
-      <header className="header">
-        <a href="/" className="logo">
+      <header>
+        {/* <a href="/" className="logo">
             <img 
             src={require('../../Assets/inhye-main-logo.svg').default} 
             alt="frontend developer logo design web graphic" style={{ width: "50%" }}/>
         </a>
         <div className="closeBtn hover-target" style={{ display: "none" }}>
           <span className="arrow"></span>
+        </div> */}
+ <div className="header menuWrapper">
+      <div className="logo-nav menuNav">
+        <div className="logo-container">
+          <a href="#">
+            <Logo className="logo" />
+          </a>
         </div>
-        <div className="menuWrapper"  style={{height: `100%`}}>
+        <ul className={click ? "nav-options active" : "nav-options menu"}>
+          <li className="option" onClick={closeMobileMenu}>
+            <Link to={'/'} className="nav-link mask">about</Link>    
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <Link to={'/projects'} className="nav-link">projects</Link>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <Link to={'/experiment'} className="nav-link">experiment</Link>
+          </li>
+          {/* <li className="option mobile-option" onClick={closeMobileMenu}>
+            <a href="#">SIGN-IN</a>
+          </li>
+          <li className="option mobile-option" onClick={closeMobileMenu}>
+            <a href="" className="sign-up">
+              darkmode
+            </a>
+          </li> */}
+        </ul>
+      </div>
+      <DarkModeToggle />
+      {/* <ul className="signin-up">
+        <li className="sign-in" onClick={closeMobileMenu}>
+          <a href="#">SIGN-IN</a>
+        </li>
+        <li onClick={closeMobileMenu}>
+          <a href="" className="signup-btn">
+            SIGN-UP
+          </a>
+        </li>
+      </ul> */}
+      <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+      </div>
+    </div>
+
+
+        {/* <div className="logo-nav">
+        <div className="logo-container">
+          <a href="/">
+            <Logo className="logo" alt="frontend developer logo design web graphic"/>
+          </a>
+        </div>
+        </div>
+        <div className="menuWrapper" style={{height: `100%`}}>
+        <div className="menuNav">
+            <ul className={click ? "nav-options active" : "nav-options menu"}>
+            <li className="option" onClick={closeMobileMenu}>
+                <Link to={'/'} className="nav-link mask">about</Link>    
+            </li>
+            <li className="option" onClick={closeMobileMenu}>
+                <Link to={'/projects'} className="nav-link">projects</Link>
+            </li>
+            <li className="option" onClick={closeMobileMenu}>
+                <Link to={'/experiment'} className="nav-link">experiment</Link>
+            </li>
+            </ul>
+        </div>
+        </div>
+        <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+        </div> */}
+
+        {/* <div className="menuWrapper"  style={{height: `100%`}}>
             <div className="menuNav">
             <div className="menu">
                 <p>
@@ -35,10 +121,9 @@ const Header = () => {
                 <p></p>
                 <div className="menuLine"></div>
                 <DarkModeToggle />
-                <MobileMenu />
             </div>
             </div>
-        </div>
+        </div> */}
         <div className="progress"></div>
 
       </header>

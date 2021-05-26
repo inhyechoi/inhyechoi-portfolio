ca6d9b7ed3e8e4ea9afb1a4db06df3a6893fb5d9
Inhye Choi <inhyechoi0404@gmail.com>
2021-05-26 12:57:17 -0700

    notfound

--- src/Pages/NotFoundPage.js
+++ src/Pages/NotFoundPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './NotFoundPage.scss'
+import { useHistory } from "react-router-dom";
 
 const NotFoundPage = () => {
+    const history = useHistory();
     return(
         <div>
             <div className="notFoundWrapper">
                 <h3>404 page not found</h3>
                 <p>sorry, but the page you are looking for does not exist.</p>
+                <button className="button back-button" onClick={() => history.goBack()}>Back</button>
             </div>
         </div>
     );
